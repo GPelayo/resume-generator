dbc9fa86555001c372a8ca9d5a6ca225336dd81a
gpelayo <gpelayo109@gmail.com>
2022-10-16 17:39:49 -0700

    Name is configurable in admin

--- landing/models.py
+++ landing/models.py
@@ -87,6 +87,10 @@ class Person(models.Model):
     first_name = models.CharField(max_length=31)
     last_name = models.CharField(max_length=31)
 
+    @property
+    def full_name(self):
+        return f'{self.first_name} {self.last_name}'
+
     def __str__(self):
         return f'{self.last_name}, {self.first_name}'
 
